08531862dbd1fe68b7757bbe35a812368cec96a2
wlznb131 <wanglong@wlznb131.local>
2015-06-30 16:00:01 +0800

    测试001
    测试002

---
+++ 测试/测试/测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by long on 15/6/30.
+//  Copyright (c) 2015年 WLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
